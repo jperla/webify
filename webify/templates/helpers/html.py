673e93e01ae2d667cb88c9b7a11673aecc39eca1
Joseph Perla @ amazon <jperla-amazon@jperla.com>
2008-12-29 15:05:10 -0800

    more html attrs

--- webify/templates/helpers/html.py
+++ webify/templates/helpers/html.py
@@ -1,5 +1,16 @@
 
+def h(text):
+    return text
+
 def a(url, text, **kwargs):
     #TODO: jperla: use kwargs as arbitrary html attributes
-    return '<a href="%(url)s">%(text)s</a>' % {'url':url, 'text':text}
+    return '<a href="%(url)s">%(text)s</a>' % {'url':url, 'text':h(text)}
 
+def h1(text):
+    return '<h1>%s</h1>' % h(text)
+
+def h2(text):
+    return '<h2>%s</h2>' % h(text)
+
+def h3(text):
+    return '<h3>%s</h3>' % h(text)
