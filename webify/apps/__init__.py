fdc79f63892b1801ec1dd78f60869f2ea325ef54
Joseph Javier Perla <notAnEmail @AT@@@ jperla.com>
2009-08-09 17:03:43 +0000

    new dispatchers

--- webify/apps/__init__.py
+++ webify/apps/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 import types
 import logging
+import wsgiref
 
 from . import standard
 
@@ -55,10 +56,14 @@ class DispatcherApp(App):
             return self.parent.wrap_url(self, url)
 
 class DispatchApp(App):
-    def __init__(self)
-        raise NotImplementedError
+    def __init__(self):
+        App.__init__(self)
 
     def __call__(self, req, p):
+        app, req = self.dispatched_app_and_request(req, p)
+        app(req, p)
+
+    def dispatched_app_and_request(req, p):
         # returns (app, req)
         raise NotImplementedError
         
@@ -72,34 +77,43 @@ class DispatchApp(App):
     def register_subapp(self, subapp):
         raise NotImplementedError
 
-    def parent_url(self, subapp, suburl):
-        url = self.wrap_url(subapp, suburl)
+    def wrap_url(self, subapp, suburl):
+        url = self.parented_url(subapp, suburl)
         if self.parent is None:
             return url
         else:
-            return self.parent.parent_url(self, url)
+            return self.parent.wrap_url(self, url)
 
-    def wrap_url(self, subapp, suburl):
+    def parented_url(self, subapp, suburl):
         raise NotImplementedError
 
 class SimpleDispatchApp(DispatchApp):
-    def __init__(self, default=None)
-        self.default = None
+    def __init__(self, default=None):
+        self.default = default
         self.apps, self.urls = {}, {}
+    
+    def default_subapp(self, *args, **kwargs):
+        assert(self.default is None)
+        def subapp_decorator(subapp):
+            subapp.parent = self
+            self.default = subapp
+            return subapp
+        return subapp_decorator
 
-    def __call__(self, req, p):
+    def dispatched_app_and_request(self, req, p):
         path_info = req.environ[u'PATH_INFO'] #for debugging
         #TODO: jperla: deep copy request here?
-        name = u'/%s' % (wsgiref.util.shift_path_info(req.environ) or u'')
+        name = u'/%s' % req.path_info.lstrip('/').split('/', 1)[0]
         apps = self.apps
         app = apps.get(name, None)
         if app is not None:
+            wsgiref.util.shift_path_info(req.environ)
             return app, req
         else:
             if self.default is not None:
                 return self.default, req
             else:
-                raise http.status.not_found()
+                raise _http.status.not_found()
 
     def register_subapp(self, subapp, path=None):
         name = (u'/%s' % subapp.func.func_name) if path is None else path
@@ -108,33 +122,39 @@ class SimpleDispatchApp(DispatchApp):
         self.apps[name] = subapp
         self.urls[subapp] = name
 
-    def wrap_url(self, subapp, suburl):
+    def parented_url(self, subapp, suburl):
         assert(subapp in self.urls or subapp == self.default)
         #TODO: jperla: fix index urls
-        return (self.urls[subapp] + controller_url).replace(u'//', u'/')
+        if subapp == self.default:
+            return suburl
+        else:
+            return (self.urls[subapp] + controller_url).replace(u'//', u'/')
 
 class BooleanDispatchApp(DispatchApp):
-    def __init__(self)
+    def __init__(self, default=None):
+        self.default = default
         self.apps = {}
+        DispatchApp.__init__(self)
 
-    def __call__(self, req, p):
+    def dispatched_app_and_request(self, req, p):
         path_info = req.environ[u'PATH_INFO'] #for debugging
         apps = self.apps
         for boolean, subapp in apps.iteritems():
-            if boolean(path_info):
+            if boolean(req):
                 return subapp, req
-        # Otherwise, no matches
-        raise http.status.not_found()
+        if self.default is not None:
+            return self.default, req
+        else:
+            raise _http.status.not_found()
 
     def register_subapp(self, subapp, boolean):
         if boolean in self.apps:
             raise Exception(u'Already dispatching to path: %s' % path)
         self.apps[boolean] = subapp
 
-    def wrap_url(self, subapp, suburl):
+    def parented_url(self, subapp, suburl):
         assert(subapp in self.apps.values())
-        #TODO: jperla: fix index urls
-        return controller_url
+        return suburl
 
 class SingleApp(DispatcherApp):
     def __init__(self):
